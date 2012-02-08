69d7a16ae4fbeda5b0e841e37b9979b3a313182d
Matt West <matt.west@developercity.co.uk>
2012-02-08 18:55:21 +0000

    Added some comments to example the euclidean distance algorithm (ruby implementation)

--- euclidean-distance/euclidean.rb
+++ euclidean-distance/euclidean.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby 
 
+# Some data to test with
 Critics = {
   'Lisa Rose' => {
     'Lady in the Water' => 2.5,
@@ -52,27 +53,32 @@ Critics = {
   }
 }
 
-# Returns a distance-based similarity score for item1 and item2
+# Returns a distance-based similarity score for p1 vs p2
 def euclidean_distance(prefs, p1, p2)
-  si = []
+  si = [] # Empty array for films that can be compared (both user's have rated)
   
-  prefs[p1].each do |item|
-    prefs[p2].each do |p2_film|
-      if item[0] == p2_film[0]
+  prefs[p1].each do |item| # Loop through each rating for p1
+    prefs[p2].each do |p2_film| # Loop through each rating for p2
+      if item[0] == p2_film[0] # If both people have rated this film add it to the films array
         si << item[0]
       end
     end
   end
   
+  # If there are no films that both people have rated stop here and return 0
   if si.length == 0
     return 0
   end
   
-  sum_of_squares = 0
+  sum_of_squares = 0 # For the sum of all the disctances
   
   si.each do |item|
+    # Calculate the distance between the two ratings in the rating graph
     sum_of_squares += (prefs[p1][item]-prefs[p2][item])**2
   end
   
+  # Add 1 to the square root of sum_of_squares and divide it all by 1.
+  # This gives a value that is large for people that are more similar
+  # 0 = Totally Unique / 1 = Identical
   return 1/(1+Math.sqrt(sum_of_squares))
 end
