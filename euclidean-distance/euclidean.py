4f9f2e4c452f08415b7d2ab9889689d89c1ede4d
Matt West <matt.west@developercity.co.uk>
2012-02-08 18:37:16 +0000

    Updated ruby implementation of euclidean distance

--- euclidean-distance/euclidean.py
+++ euclidean-distance/euclidean.py
@@ -1,3 +1,24 @@
+# A dictionary of movie critics and their ratings of a small
+# set of movies
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 
+ 'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 
+ 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 
+ 'You, Me and Dupree': 3.5}, 
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+ 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+ 'The Night Listener': 4.5, 'Superman Returns': 4.0, 
+ 'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 
+ 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 2.0}, 
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+ 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+
 from math import sqrt
 
 # Returns a distance-based similarity score for item1 and item2
@@ -10,6 +31,8 @@ def euclidean_distance(prefs, item1, item2):
 	if len(si) == 0: return 0
 	
 	sum_of_squares = sum([pow(prefs[item1][item]-prefs[item2][item],2) for item in si])
+	
+	print sum_of_squares
 	
 	return 1/(1+sqrt(sum_of_squares))
 	
--- euclidean-distance/euclidean.rb
+++ euclidean-distance/euclidean.rb
@@ -1,8 +1,80 @@
+#!/usr/bin/env ruby 
+
+Critics = {
+  'Lisa Rose' => {
+    'Lady in the Water' => 2.5,
+    'Snakes on a Plane' => 3.5,
+    'Just My Luck'  => 3.0,
+    'Superman Returns' => 3.5,
+    'You, Me and Dupree' => 2.5,
+    'The Night Listener' => 3.0
+  },
+  'Gene Seymour' => {
+    'Lady in the Water' => 3.0,
+    'Snakes on a Plane' => 3.5,
+    'Just My Luck'  => 1.5,
+    'Superman Returns' => 5.0,
+    'You, Me and Dupree' => 3.5,
+    'The Night Listener' => 3.0
+  },
+  'Michael Phillips' => {
+    'Lady in the Water' => 2.5,
+    'Snakes on a Plane' => 3.0,
+    'Superman Returns' => 3.5,
+    'The Night Listener' => 4.0
+  },
+  'Claudia Puig' => {
+    'Snakes on a Plane' => 3.5,
+    'Just My Luck'  => 3.0,
+    'Superman Returns' => 4.0,
+    'You, Me and Dupree' => 2.5,
+    'The Night Listener' => 4.5
+  },
+  'Mick LaSalle' => {
+    'Lady in the Water' => 3.0,
+    'Snakes on a Plane' => 4.0,
+    'Just My Luck'  => 2.0,
+    'Superman Returns' => 3.0,
+    'You, Me and Dupree' => 2.0,
+    'The Night Listener' => 3.0
+  },
+  'Jack Matthews' => {
+    'Lady in the Water' => 3.0,
+    'Snakes on a Plane' => 4.0,
+    'Superman Returns' => 5.0,
+    'You, Me and Dupree' => 3.5,
+    'The Night Listener' => 3.0
+  },
+  'Toby' => {
+    'Snakes on a Plane' => 4.5,
+    'Superman Returns' => 4.0,
+    'You, Me and Dupree' => 1.0
+  }
+}
+
 # Returns a distance-based similarity score for item1 and item2
-def euclidean_distance(p1,p2)
-  sum_of_squares = 0
-  p1.each_with_index do |p1_coord,index|
-    sum_of_squares += (p1_coord - p2[index])**2
+def euclidean_distance(prefs, p1, p2)
+  si = []
+  
+  prefs[p1].each do |item|
+    prefs[p2].each do |p2_film|
+      if item[0] == p2_film[0]
+        si << item[0]
+      end
+    end
   end
-  Math.sqrt(sum_of_squares)
+  
+  if si.length == 0
+    return 0
+  end
+  
+  sum_of_squares = 0
+  
+  si.each do |item|
+    item_sum = (prefs[p1][item]-prefs[p2][item])**2
+    puts item_sum
+    sum_of_squares += item_sum
+  end
+  
+  return 1/(1+Math.sqrt(sum_of_squares))
 end
